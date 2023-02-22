a8b3022dfefb24ac2903a397d9ee31a7413f0083
SvMa <svetakrivaya2002@gmail.com>
2023-02-22 09:32:49 +0400

    Two

---
+++ database/factories/CardFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Card>
+ */
+class CardFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name'=>$this->faker->text(15),
+            'post_id'=>Post::inRandomeOrder()->all()
+        ];
+    }
+}

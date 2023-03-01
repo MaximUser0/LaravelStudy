905b2f96fe9e878c13363af4d9b002b0ddf68e3d
MaximUser0 <makcim.o.d@gmail.com>
2023-03-01 13:29:14 +0400

    View div

--- resources/views/desk.blade.php
+++ resources/views/desk.blade.php
@@ -4,13 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/app.css">
     <title>Доски</title>
 </head>
 <body>
-    <ul>
+    <div>
       @foreach ($desks as $desk)
-      <li>{{ $desk->id . " " . $desk->name }}</li>
+      <div class="block">{{ $desk->id . " " . $desk->name }}</div>
         @endforeach
-    </ul>
+    </div>
 </body>
 </html>
+<style>
+    .block{
+    background-color: rgb(159, 142, 255);
+    margin: 2vw;
+    padding: 2vw;
+    width: 20vw;
+}
+</style>
